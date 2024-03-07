3db32a3d1c80731678a7dcb3c681e19d0ec51a9a
yurii.sytnyk@spd-ukraine.com <yurii.sytnyk@spd-ukraine.com>
2024-03-07 21:37:22 +0200

    feat: updated carousel image layout

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,16 +1,22 @@
 import Swiper from "swiper";
+import { Scrollbar, Keyboard } from "swiper/modules";
+
 import "swiper/css/bundle";
 
 export function initCarousel() {
   new Swiper(".swiper", {
     slidesPerView: "auto",
+    direction: "horizontal",
     spaceBetween: 30,
-    grabCursor: true,
+    modules: [Scrollbar, Keyboard],
+    keyboard: {
+      enabled: true,
+    },
     scrollbar: {
       el: ".swiper-scrollbar",
       hide: true,
       draggable: true,
-      dragSize: 50,
+      dragSize: 380,
     },
   });
 }
