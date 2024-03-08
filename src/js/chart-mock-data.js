93f873fddb9f2bc961b575064b0b7e772c6212ba
yurii.sytnyk@spd-ukraine.com <yurii.sytnyk@spd-ukraine.com>
2024-03-08 15:24:17 +0200

    feat: updated mock data

--- src/js/chart-mock-data.js
+++ src/js/chart-mock-data.js
@@ -8,26 +8,29 @@ const borderRadius = { topLeft: radius, topRight: radius, bottomLeft: radius, bo
 export const chartMockData = [
   {
     label: "Valores para aluguel",
-    data: [0, 8],
+    data: [5e3, 8e3, 20e3, 30e3, 52e3, 28e3, 30e3, 10e3, 32e3],
     backgroundColor: colors.blue[100],
     borderRadius,
     borderSkipped: false,
     barThickness: 32,
+    base: 0,
   },
   {
     label: "Valores para venda",
-    data: [0, 23],
+    data: [10e3, 23e3, 28e3, 73e3, 110e3, 51e3, 55e3, 25e3, 52e3],
     backgroundColor: colors.blue[75],
     borderRadius,
     borderSkipped: false,
     barThickness: 32,
+    base: 0,
   },
   {
     label: "valor m2 por região",
-    data: [0, 48],
+    data: [48e3, 51e3, 55e3, 90e3, 145e3, 60e3, 48e3, 90e3, 110e3],
     backgroundColor: colors.blue[15],
     borderRadius,
     borderSkipped: false,
     barThickness: 32,
+    base: 0,
   },
 ];
