63e1226e97489ab3ca98d45d7809e9b78e39c36b
yurii.sytnyk@spd-ukraine.com <yurii.sytnyk@spd-ukraine.com>
2024-03-11 11:38:36 +0200

    feat: disabled chart tooltip

--- src/js/chart.js
+++ src/js/chart.js
@@ -81,6 +81,9 @@ export function initChart(datasets) {
         },
       },
       plugins: {
+        tooltip: {
+          enabled: false,
+        },
         legend: {
           position: "bottom",
           labels: {
